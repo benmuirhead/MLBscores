d0aabeb8c71a41d6fdd4ae21faddc7fc4c8fa2db
Ben Muirhead <bencmuirhead@gmail.com>
2015-04-27 22:01:43 -0400

    minor change

--- testRun.java
+++ testRun.java
@@ -30,7 +30,8 @@ public class testRun extends JFrame{
 		GameDay testGames = new GameDay(2015,03,23);
 		System.out.println(testGames.toString());
 		List<Game> gameList = testGames.getGames();
-		JPanel lineTest = Utility.drawLineScore(gameList.get(0));his.setSize(500, 500);
+		JPanel lineTest = Utility.drawLineScore(gameList.get(0));
+		this.setSize(500, 500);
 		this.add(lineTest);
 		this.setVisible(true);
 	}
