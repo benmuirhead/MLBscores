a472e53bef4274327432e7289b7098d032ab1088
Ben Muirhead <bencmuirhead@gmail.com>
2015-05-02 21:33:54 -0400

    Add class Gui. will be used to create and hold all JPanels. I will eventually move all of the graphics out of test Run and into Gui

---
+++ Gui.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ben
+ *
+ *This GUI will create a large JPanel that will hold all the panels, dropdowns, etc
+ *Structure:
+ *	JPanel windowFrame
+ *		Date Dropdown
+ *		All Scores Panel - refreshes each time a new date is selected
+ *			Will Contain 3 sections: AL NL Interleague
+ *				Each Section will hold all the relevant games
+ *		Indiv Game Panel - refreshes each time a new game is selected
+ *			When a game is selected, it shows the full 9 inning plue RHE for each team
+ *
+ */
+public class Gui {
+JPanel windowFrame
+}
